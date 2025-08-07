8c2492b6e031f573f7c4aefaf1ba050f81712582
msamahaa <msamaha@uolinc.com>
2025-08-07 14:57:32 -0300

    implementa crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'app.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase
+  await Firebase.initializeApp();
+
+  // Captura erros do Flutter
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Captura erros fora da árvore de widgets
+  runZonedGuarded(() {
+    runApp(const App());
+  }, FirebaseCrashlytics.instance.recordError);
 }
